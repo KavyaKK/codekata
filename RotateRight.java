4d76d6111377756dc3964a6230df8115fb118583
KavyaKK <kcube07@gmail.com>
2017-06-22 17:20:53 +0530

    Update RotateRight.java

--- RotateRight.java
+++ RotateRight.java
@@ -1,29 +1,31 @@
 import java.util.Scanner;
 
-public class ReverseDigits
+public class RotateRight 
 {
-	public static void main(String[] args) 
-    {
-     int n,r;
-     Scanner in= new Scanner(System.in);
-     System.out.println("Enter the number to be reversed : ");
-     String rev=new String();
-     try{
-     n=in.nextInt();
-     int b=n;
-     while(b!=0)
-     {
-        r=b%10;
-        b=b/10;
-        rev=rev+r;
-     }
-     int revnum=Integer.parseInt(rev);
-     System.out.println("The reversed number is "+revnum);
-     in.close();
-     }
-     catch (Exception e)
-     {
-         System.out.println("Invalid input.");
-     }
-    }
+	public static void main(String[] args)
+	{
+		int n;
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter the length of array : ");
+		n=in.nextInt();
+		int [] arr=new int[n];
+		int [] rot_arr=new int[n];
+		System.out.println("Enter the elements : ");
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=in.nextInt();
+		}
+		System.out.println("Enter the number of right rotations : ");
+		int k =in.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			rot_arr[i]=arr[(i+k-1)%n];
+		}
+		for(int i=0;i<n;i++)
+		{
+			System.out.print(rot_arr[i]+" ");
+		}
+		in.close();
+			
+	}
 }
