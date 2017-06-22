4a10f3fedb2854c6027ac3b207c5b5aae79b6638
KavyaKK <kcube07@gmail.com>
2017-06-22 17:13:57 +0530

    Update ReverseDigits.java

--- ReverseDigits.java
+++ ReverseDigits.java
@@ -1,18 +1,29 @@
 import java.util.Scanner;
 
-public class Factorial
+public class ReverseDigits
 {
-	public static void main(String[] args)
-	{
-		int num,fact=1;
-		Scanner in=new Scanner(System.in);
-		System.out.println("Enter a number to find its factorial");
-		num=in.nextInt();
-		for(int i=1;i<=num;i++)
-		{
-			fact=fact*i;
-		}
-		System.out.println("The factorial of "+num+" is "+fact);
-		in.close();
-	}
+	public static void main(String[] args) 
+    {
+     int n,r;
+     Scanner in= new Scanner(System.in);
+     System.out.println("Enter the number to be reversed : ");
+     String rev=new String();
+     try{
+     n=in.nextInt();
+     int b=n;
+     while(b!=0)
+     {
+        r=b%10;
+        b=b/10;
+        rev=rev+r;
+     }
+     int revnum=Integer.parseInt(rev);
+     System.out.println("The reversed number is "+revnum);
+     in.close();
+     }
+     catch (Exception e)
+     {
+         System.out.println("Invalid input.");
+     }
+    }
 }
